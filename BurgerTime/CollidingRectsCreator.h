6cdbd87fd6a426e54ffea2bf5c4d5f74c37a9639
john <john@inkling.com>
2013-01-26 13:32:54 -0800

    Adds heart background, and proper colliding rects, needs optimization and tweaking to where we spawn units and enemies to keep them from spawning outside the heart

---
+++ BurgerTime/CollidingRectsCreator.h
@@ -0,0 +1,17 @@
+//
+//  CollidingRectsCreator.h
+//  BurgerTime
+//
+//  Created by John Detloff on 1/26/13.
+//  Copyright (c) 2013 GlobalGameJam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CollidingRectsCreator : NSObject
+
+
++ (NSArray *)collidingRectsForHeartWithScale:(CGFloat)scale;
+
+
+@end
