08d4f45b418bffd72ae747f97ff2058009372bb8
laaph <laaph@laaph.com>
2013-01-26 11:53:43 -0800

    Added Wave object

---
+++ BurgerTime/Wave.h
@@ -0,0 +1,13 @@
+//
+//  Wave.h
+//  BurgerTime
+//
+//  Created by stanza on 1/26/13.
+//  Copyright (c) 2013 GlobalGameJam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Wave : NSObject
+
+@end
---
+++ BurgerTime/Wave.m
@@ -0,0 +1,13 @@
+//
+//  Wave.m
+//  BurgerTime
+//
+//  Created by stanza on 1/26/13.
+//  Copyright (c) 2013 GlobalGameJam. All rights reserved.
+//
+
+#import "Wave.h"
+
+@implementation Wave
+
+@end
