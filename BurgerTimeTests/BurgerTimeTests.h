67fcd18c3f3d4e5b7dbb97e2a08d65b8cb6a09e3
John Detloff <john@inkling.com>
2013-01-25 20:03:15 -0800

    Initial Commit

---
+++ BurgerTimeTests/BurgerTimeTests.h
@@ -0,0 +1,13 @@
+//
+//  BurgerTimeTests.h
+//  BurgerTimeTests
+//
+//  Created by John Detloff on 1/25/13.
+//  Copyright (c) 2013 GlobalGameJam. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface BurgerTimeTests : SenTestCase
+
+@end
---
+++ BurgerTimeTests/BurgerTimeTests.m
@@ -0,0 +1,32 @@
+//
+//  BurgerTimeTests.m
+//  BurgerTimeTests
+//
+//  Created by John Detloff on 1/25/13.
+//  Copyright (c) 2013 GlobalGameJam. All rights reserved.
+//
+
+#import "BurgerTimeTests.h"
+
+@implementation BurgerTimeTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in BurgerTimeTests");
+}
+
+@end
