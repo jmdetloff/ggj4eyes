9c11b5cd8a70ba480a84251ef32f2b89cacfb4fb
mdsa <mdsa@funzio.com>
2013-01-26 23:30:29 -0800

    import conversion script

--- db/convert.py
+++ db/convert.py
@@ -11,10 +11,13 @@ JSON_FILE_EXTN = '.json'
 DB_DIR = os.path.dirname(os.path.realpath(__file__))
 INPUT_DIR = os.path.join(DB_DIR, 'input')
 OUTPUT_DIR = os.path.join(DB_DIR, 'data')
+OUTPUT_FILE_NAME = os.path.join(OUTPUT_DIR, 'everything.json')
 
 FILES = filter(lambda x:x.endswith(TEXT_FILE_EXTN), os.listdir(INPUT_DIR))
 
+megadict = {}
 for fn in FILES:
+	fn_no_ext = fn[:-len(TEXT_FILE_EXTN)]
 	f = open(os.path.join(INPUT_DIR, fn), 'r')
 	hdrline = f.readline().strip()
 	typeline = f.readline().strip()
@@ -26,7 +29,7 @@ for fn in FILES:
 
 	types = typeline.split(DELIMITER)
 
-	ret = {}
+	ret = []
 	i = 0
 	for line in lines:
 		line = line.strip()
@@ -39,12 +42,14 @@ for fn in FILES:
 				d[fields[j]] = entries[j]
 			elif types[j] == 'dict':
 				d[fields[j]] = json.loads(entries[j])
-		ret[str(i)] = d
+		ret.append(d)
 		i += 1
-	s = json.dumps(ret)
+	megadict[fn_no_ext] = ret
+	# s = json.dumps(ret)
 
-	outfn = os.path.join(OUTPUT_DIR, fn[:-len(TEXT_FILE_EXTN)]+JSON_FILE_EXTN)
-	g = open(outfn, 'w')
-	g.write(s)
-	g.close()
-	print 'Wrote to '+outfn+'.'
+# outfn = os.path.join(OUTPUT_DIR, fn[:-len(TEXT_FILE_EXTN)]+JSON_FILE_EXTN)
+g = open(OUTPUT_FILE_NAME, 'w')
+g.write(json.dumps(megadict))
+g.close()
+# print 'Wrote to '+outfn+'.'
+print 'Exported data.'
