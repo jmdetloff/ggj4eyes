5864ba57b62c9eed168904ec176434cbe916c679
john <john@inkling.com>
2013-01-26 17:45:21 -0800

    Pinch to zoom functionality

---
+++ BurgerTime/PanlessScrollView.h
@@ -0,0 +1,13 @@
+//
+//  PanlessScrollView.h
+//  BurgerTime
+//
+//  Created by John Detloff on 1/26/13.
+//  Copyright (c) 2013 GlobalGameJam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PanlessScrollView : UIScrollView
+
+@end
